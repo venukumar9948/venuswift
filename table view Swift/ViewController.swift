72d4783d23cc117e8f08bbd16e364c2a7caceff3
student <student@achiever-84.local>
2017-11-17 10:42:12 +0530

    Initial Commit

---
+++ table view Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  table view Swift
+//
+//  Created by student on 11/17/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
